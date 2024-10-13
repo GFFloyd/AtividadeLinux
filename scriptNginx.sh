dbc24013431a0fe598bb5d683d6417ea942b207e
GFFloyd <gfaracovip@gmail.com>
2024-10-13 13:18:39 -0300

    bug do cron corrigido + algumas modificações no script

--- scriptNginx.sh
+++ scriptNginx.sh
@@ -1,20 +1,25 @@
-#!usr/bin/bash
+#!/bin/bash
 
 #Diretório para onde vai ficar o log
-path=/home/$USER/log;
+#Esta parte tem que ser hardcoded, sem a variável $USER, pois o cron dá erro
+
+path=/home/gfaraco/log;
 file=$path/logfile.log;
 
 #Cria o diretório de onde vai ficar o log, se ele não existir
+
 if [ ! -d $path ]; then
 	mkdir -p $path
 fi
 
 #Cria o arquivo de log no diretório específico, se o arquivo não existir
-if [ ! -f $path/logfile.log ]; then
+
+if [ ! -f $file ]; then
 	touch $file;
 fi
 
 #Testar se o serviço nginx está online ou offline e tratar a string do outputi
+
 if service nginx status | grep -q '* nginx is not running'; then
 	service="Serviço Nginx está offline";
 else service="Serviço Nginx está online";
@@ -22,6 +27,7 @@ fi
 
 #Depois de validado o output do serviço, concatenar o resultado com a data atual e mandar
 #para o arquivo de log
+
 outputString="$(date '+%d-%m-%Y %H:%M:%S') -"; 
 echo "$outputString $service" >> $file;
 
