c86aeff30eeed28831c4760915133e02b1c64017
GFFloyd <gfaracovip@gmail.com>
2024-10-13 10:34:30 -0300

    script funcionando

--- scriptNginx.sh
+++ scriptNginx.sh
@@ -3,24 +3,25 @@
 #Diretório para onde vai ficar o log
 path=/home/$USER/log;
 file=$path/logfile.log;
-service=service nginx status;
-#Testa para ver se o diretório de onde vai ficar o log existe e se não existir ele é criado
+
+#Cria o diretório de onde vai ficar o log, se ele não existir
 if [ ! -d $path ]; then
 	mkdir -p $path
 fi
 
-#Cria o arquivo de log no diretório específico
+#Cria o arquivo de log no diretório específico, se o arquivo não existir
 if [ ! -f $path/logfile.log ]; then
 	touch $file;
-else echo "file not created";
 fi
 
-#Testar se o serviço nginx está online ou offline e tratar a string do output
-if [ $service == "* " ]
-
+#Testar se o serviço nginx está online ou offline e tratar a string do outputi
+if service nginx status | grep -q '* nginx is not running'; then
+	service="Serviço Nginx está offline";
+else service="Serviço Nginx está online";
+fi
 
 #Depois de validado o output do serviço, concatenar o resultado com a data atual e mandar
 #para o arquivo de log
 outputString="$(date '+%d-%m-%Y %H:%M:%S') -"; 
-echo $outputString >> $file;
+echo "$outputString $service" >> $file;
 
